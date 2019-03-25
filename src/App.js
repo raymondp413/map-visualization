060a73d260300430bbae326b613e19f0e9b762b3
Raymond Simmons <raymond@neonsky.com>
2019-03-24 22:35:34 -0500

    Modified App component to mount ControlCenter component

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ import CssBaseline from "@material-ui/core/CssBaseline";
 import "react-toastify/dist/ReactToastify.css";
 import Header from "./components/Header";
 import Wrapper from "./components/Wrapper";
-import NowWhat from "./components/NowWhat";
+import ControlCenter from "./components/ControlCenter";
 
 const store = createStore();
 const theme = createMuiTheme({
@@ -33,7 +33,7 @@ const App = props => (
     <Provider store={store}>
       <Wrapper>
         <Header />
-        <NowWhat />
+        <ControlCenter />
         <ToastContainer />
       </Wrapper>
     </Provider>
